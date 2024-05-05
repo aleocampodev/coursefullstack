7658882b37f3a8eed94c8497382ce1ca0482f8bf
Aleja Ocampo <omaira.527@hotmail.com>
2024-05-04 22:25:43 -0500

    solution exercise 1.4 part 1

--- part 1/courseInfo/src/components/Content.jsx
+++ part 1/courseInfo/src/components/Content.jsx
@@ -4,9 +4,11 @@ import Part from './Part'
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1} exercises={props.exercises1}/>
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3}/>
+     { 
+        props.parts.map((item,index) => (
+            <Part part={item.name} exercises={item.exercises} key={index}/>
+        ))
+     }
     </>
   )
 }
--- part 1/courseInfo/src/components/Total.jsx
+++ part 1/courseInfo/src/components/Total.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Total = ({exercises1, exercises2, exercises3}) => {
+const Total = (props) => {
+    const totalExercises = props.parts.reduce((total, part) => total + part.exercises, 0);
   return (
     <>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises {totalExercises}</p>
     </>
   )
 }
