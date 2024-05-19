a4a8b95961d658a0ea78074d6f996b3717413425
Aleja Ocampo <omaira.527@hotmail.com>
2024-05-18 20:57:26 -0500

    solution exercise 1.8 part 1

--- part 1/unicafe/src/App.jsx
+++ part 1/unicafe/src/App.jsx
@@ -1,8 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Header from './components/Header'
 import Experiences from './components/Experiences'
-import Statistics from './components/Statistics'
 import './App.css'
+
+const Statistics = (props) => {
+  return <div>{props.text} {props.value}</div>
+}
 
 function App() {
   const [good, setGood] = useState(0)
@@ -11,17 +14,27 @@ function App() {
   const title = 'give feedback'
   const all = good + neutral + bad;
   const average = all !== 0 ? (good - bad) / all : 0;
-  const positivePercentage = all !== 0 || good !== 0? (good / all) * 100: 0;
+  const positivePercentage = all !== 0 || good !== 0 ? (good / all) * 100 : 0;
 
   return (
     <>
-      <Header title={title}/>
+      <Header title={title} />
       <div className="container-buttons">
-        <Experiences nameButton={'good'} counterFeedbackButton={good} setCounterFeedBackButton={setGood}/>
-        <Experiences nameButton={'neutral'} counterFeedbackButton={neutral} setCounterFeedBackButton={setNeutral}/>
-        <Experiences nameButton={'bad'} counterFeedbackButton={bad} setCounterFeedBackButton={setBad}/>
+        <Experiences nameButton={'good'} counterFeedbackButton={good} setCounterFeedBackButton={setGood} />
+        <Experiences nameButton={'neutral'} counterFeedbackButton={neutral} setCounterFeedBackButton={setNeutral} />
+        <Experiences nameButton={'bad'} counterFeedbackButton={bad} setCounterFeedBackButton={setBad} />
       </div>
-      <Statistics  good={good} bad={bad} neutral={neutral} all={all} average={average} positive={`${positivePercentage.toFixed(2)}%`}/>
+      <div>
+        <h2><strong>statistics</strong></h2>
+        <Statistics text={'good'} value={good} />
+        <Statistics text={'neutral'} value={neutral} />
+        <Statistics text={'bad'} value={bad} />
+        <Statistics text={'all'} value={all} />
+        <Statistics text={'average'} value={average} />
+        <Statistics text={'positive'} value={`${positivePercentage.toFixed(2)}%`} />
+      </div>
+
+
     </>
   )
 }
